fca7b8ed162fb2ad3c32ba94eb4c7a9bbe7fce41
yemmir MG <alexgr999a@gmail.com>
2026-07-11 17:14:13 -0500

    configurar smtp con puerto 587

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,15 +1,14 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "CARGADA ✅" : "NO EXISTE ❌");
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 module.exports = transporter;
