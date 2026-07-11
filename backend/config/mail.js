35c01cb02ad1a8230d9d7be9e2ef0c6579a5ccbd
yemmir MG <alexgr999a@gmail.com>
2026-07-11 17:08:45 -0500

    Arregle el archvio confing con un error de dircciones

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "CARGADA ✅" : "NO EXISTE ❌");
+
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
